165fac3b111d1bf5f3de9741a2401b0f1c6b8656
almeald <almenaldana@gmail.com>
2020-10-27 01:54:44 -0500

    funcionalidad del mensaje de error para 3 inputs

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-const inputFname = document.querySelector("#fName");
-const inputLname = document.querySelector("#lName");
-const inputEmail = document.querySelector("#email");
-const inputPassword = document.querySelector("#password");
+const form = document.getElementById("form");
+console.log(form);
 
 const msgFname = document.querySelector(".fName");
 const msgLname = document.querySelector(".lName");
 const msgEmail = document.querySelector(".email");
 const msgPassword = document.querySelector(".password");
+const inputPassword = form["password"].value;
+const errorImg = document.querySelector("img");
 
-const btnSend = document.querySelector(".btnSend");
-
-btnSend.addEventListener("click", (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  // console.log(inputEmail);
+  const inputFname = form["fName"].value;
+  const inputLname = form["lName"].value;
+  const inputEmail = form["email"].value;
+  const inputPassword = form["password"].value;
 
-  if (!inputFname.value) {
-    msgFname.textContent = "First Name Cannot be empty";
+  console.log(inputFname, inputLname, inputEmail, inputPassword);
+
+  if (inputFname === "") {
+    addError("fName", "First Name Cannot be empty");
   }
-
-  if (inputLname.value === "")
-    msgLname.textContent = "Last Name Cannot be empty";
-
-  if (inputEmail.value === "")
-    msgEmail.textContent = "Looks like this is not an email";
-  inputEmail.value = "email@example.com";
-
-  if (inputPassword.value === "")
-    msgPassword.textContent = "Password cannot be empty";
+  if (inputLname === "") {
+    addError("lName", "Last Name Cannot be empty");
+  }
+  if (validEmail(inputEmail)) {
+    addError("email", "Looks like this is not an email");
+  }
+  if (inputPassword === "") {
+    addError("password", "Password cannot be empty");
+  }
 });
+
+function addError(input, text) {
+  const small = form[input].parentNode.querySelector("small");
+  small.innerText = text;
+  small.style.opacity = 1;
+}
 
 function validEmail(email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
